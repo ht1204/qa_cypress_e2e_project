30cdd30599cfe3d0724039d368fa54e48c79643a
Hector Triana <ptb1204@hotmail.com>
2026-04-11 21:47:08 -0500

    fix: replace node-sass with dart sass for node 18 compat

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -36,7 +36,12 @@ module.exports = (envVars) => {
           use: [
             "style-loader", // creates style nodes from JS strings
             "css-loader", // translates CSS into CommonJS
-            "sass-loader", // compiles Sass to CSS, using Node Sass by default
+            {
+              loader: "sass-loader",
+              options: {
+                implementation: require("sass"),
+              },
+            },
           ],
         },
       ],
